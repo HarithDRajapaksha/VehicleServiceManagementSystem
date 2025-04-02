45d4202cd7c6b55abb5f3a1def6a9b7887250710
HarithDRajapaksha <harithdevinda8@gmail.com>
2025-04-02 22:18:48 +0530

    First commit
    Updated the code with
    Dashboard

--- vsms/src/App.jsx
+++ vsms/src/App.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
 import './App.css'
+import HomePage from './Pages/Home page/HomePage'
+import Loginpage from './Pages/Login Page/Loginpage'
+import RegistrationPage from './Pages/Registration Page/RegistrationPage'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React from 'react' 
+import Dashboard from './Pages/Dashboard/Main Dashboard/Dashboard'
+import EnquiryForm from './Pages/Dashboard/Enquiry Form/EnquiryForm'
+import ServiseRequestForm from './Pages/Dashboard/Service Request Form/ServiseRequestForm'
+import MyProfile from './Pages/Dashboard/My Profile/MyProfile'
 
 function App() {
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/registration" element={<RegistrationPage />} />
+          <Route path="/login" element={<Loginpage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/dashboard/enquiry" element={<EnquiryForm />} />
+          <Route path="/dashboard/service-request" element={<ServiseRequestForm />} />
+          <Route path="/dashboard/my-profile" element={<MyProfile />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ vsms/src/Pages/Home page/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './homePage.css'
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>hi home page</h1>
+    </div>
+  )
+}
